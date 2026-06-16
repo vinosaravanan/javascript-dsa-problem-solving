9e68771609db91f0288ba88479be9e7a6bdf15bf
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-16 21:27:01 +0530

    dublicate

--- String/printduplicateChar.js
+++ String/printduplicateChar.js
@@ -5,25 +5,43 @@
 
 /// 4 Approaches
 
-
 //// Using Plain Object(hash map)
 
-function PrintDublicateWithObj(str){
-   let charObj = {};
-   let Dublicate = [];
+function PrintDublicateWithObj(str) {
+  let charObj = {};
+  let Dublicate = [];
 
-   for (const char of str) {
-       charObj[char] = (charObj[char] || 0) + 1
-   }
- 
-   for (const char in charObj) {
-       if (charObj[char] > 1) {
-         Dublicate.push(char)
-       }
-   }
-   return Dublicate
+  for (const char of str) {
+    charObj[char] = (charObj[char] || 0) + 1;
+  }
 
+  for (const char in charObj) {
+    if (charObj[char] > 1) {
+      Dublicate.push(char);
+    }
+  }
+  return Dublicate;
+}
+console.log("Print Dublicate With Obj", PrintDublicateWithObj("programming"));
+
+/////////// Aproche 2 Using a Array ;
+
+function PrintDublicateWithArray(str) {
+  let charSeen = [];
+  let dublicate = [];
+  for (const char of str) {
+    if (charSeen.includes(char)) {
+      if (!dublicate.includes(char)) {
+        dublicate.push(char);
+      }
+    } else {
+      charSeen.push(char);
+    }
+  }
+  return dublicate
 }
 
-
-console.log('Print Dublicate With Obj',PrintDublicateWithObj("programming"))
+console.log(
+  "Print Dublicate With Array",
+  PrintDublicateWithArray("programming"),
+);
