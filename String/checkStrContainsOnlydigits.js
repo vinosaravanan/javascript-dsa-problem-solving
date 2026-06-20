cd06ec392f971d14670cdee5a087ea7248bdc082
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-20 23:39:43 +0530

    checkStrContainsOnlydigits.js

--- String/checkStrContainsOnlydigits.js
+++ String/checkStrContainsOnlydigits.js
@@ -12,3 +12,21 @@ function isOnlyDigits(str) {
 
 console.log('Using Regular Expression',isOnlyDigits("123456"));
 console.log('Using Regular Expression',isOnlyDigits("123456s"));
+
+/// 2. Using for loop and charCodeAt
+
+function isOnlyDigitsWithcharCodeAt(str) {
+    if (str.length === 0) return false;
+
+    for (let i = 0; i < str.length; i++) {
+        const code = str.charCodeAt(i);
+        if (code < 48 || code > 57) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log('Using for loop and charCodeAt',isOnlyDigitsWithcharCodeAt("123456"));
+console.log('Using for loop and charCodeAt',isOnlyDigitsWithcharCodeAt("123456R"));
+
