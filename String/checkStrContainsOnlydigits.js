0a9e5a9af763179954809cc4946146931aadba5b
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-20 23:35:45 +0530

    checkStrContainsOnlydigits.js

--- String/checkStrContainsOnlydigits.js
+++ String/checkStrContainsOnlydigits.js
@@ -2,5 +2,13 @@
 //// input - "123456"
 /// output - true
 
+/// 3 Approches
 
+///  1. Using Regular Expression
 
+function isOnlyDigits(str) {
+    return /^\d+$/.test(str);
+}
+
+console.log('Using Regular Expression',isOnlyDigits("123456"));
+console.log('Using Regular Expression',isOnlyDigits("123456s"));
