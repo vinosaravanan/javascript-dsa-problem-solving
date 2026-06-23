322cd9e5e8ca9aceb75dc1dfbfcfa1e084069a79
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-23 23:22:08 +0530

    countVowelsAndConsonants with brute Force

---
+++ String/CountVowelsAndConsonants.js
@@ -0,0 +1,34 @@
+/////      How do you count the number of vowels and consonants in a  given string?
+
+//// inputStr - programming;
+//// vowels - (3) - o, a, i
+//// Consonants - (8) -  p, r, g, r, m, m, n, g
+
+//// 3 approches
+
+/////  1, Brute Force Technique
+function countVowelsAndConsonants(str) {
+    let vowels = 0, consonants = 0;
+    const lowerStr = str.toLowerCase();
+
+    for (let i = 0; i < lowerStr.length; i++) {
+        const char = lowerStr[i];
+        if (char >= 'a' && char <= 'z') {
+            if (
+                char === 'a' ||
+                char === 'e' ||
+                char === 'i' ||
+                char === 'o' ||
+                char === 'u'
+            ) {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    return { vowels, consonants };
+}
+
+console.log('Brute Force Technique', countVowelsAndConsonants('programming'))
