ace966ed3f05f4d6c4d24f668002538583317aa4
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-24 23:16:47 +0530

    CounttheOccurrencer with UsingRegex

--- String/CounttheOccurrenceOfgivenCharacter.js
+++ String/CounttheOccurrenceOfgivenCharacter.js
@@ -1,9 +1,11 @@
 /////          How do you count the occurrence of a given character in a string?
 //// input - programming , target - g
 //// output - 2
+//// 4 Approche
 
-let str = "programming";
-let target = "g";
+/// 1. Using a for...of Loop (Best for Interviews)
+let str1 = "programming";
+let target1 = "g";
 
 function CountOccurrenceStr(str, target) {
   let count = 0;
@@ -18,5 +20,15 @@ function CountOccurrenceStr(str, target) {
 }
 console.log(
   "CountOccurrenceStr with For Of Loop",
-  CountOccurrenceStr(str, target),
+  CountOccurrenceStr(str1, target1),
 );
+
+/// 2. Using Regex (Short & Powerful)
+let str2 = 'banana';
+let target2 = 'a'
+function countCharwithUsingRegex(str, target) {
+    const matches = str.match(new RegExp(target, "g"));
+    return matches ? matches.length : 0;
+}
+console.log('countChar with usingRegex', countCharwithUsingRegex(str2, target2));
+
