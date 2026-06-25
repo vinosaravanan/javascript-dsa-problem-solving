5739b95c4c0fdac6b3bc82865402f4a6b0b11d30
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-25 23:07:20 +0530

    PalindromeCheck

--- String/PalindromeCheck.js
+++ String/PalindromeCheck.js
@@ -1,7 +1,25 @@
-/////       How do you check if a given string is a  palindrome ?
+/////         How do you check if a given string is a  palindrome ?
+
+///// A **palindrome** is a word or sentence that reads the **same backward as forward**,
+// like: `"madam"`, `"racecar"`, `"abba"`
+
 //// input - "madam" | output - true;
 //// input - "hello" | output - false;
 
+//// 3 Approches
 
+//// 1. Two-Pointer Technique (Best for Interviews)
 
+function CheckPalindrom(str) {
+  let left = 0;
+  let right = str.length - 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+  }
+  return true;
+}
 
+console.log('CheckPalindrom with Two-Pointer Technique',CheckPalindrom("madam"));
+console.log('CheckPalindrom with Two-Pointer Technique',CheckPalindrom("hello"));
