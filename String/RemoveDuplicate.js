d985732bfb230784aa42897e3152a83d7e147591
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-29 21:01:29 +0530

    RemoveDuplicate with Object

--- String/RemoveDuplicate.js
+++ String/RemoveDuplicate.js
@@ -4,23 +4,40 @@
 
 //// 4 approches
 
-
 //// 1. Using Set (Fastest and Clean)
-
 function RemoveDuplicateWithSet(str) {
-    return [...new Set(str)].join('')
+  return [...new Set(str)].join("");
 }
-console.log('Using Set (Fastest and Clean) |', RemoveDuplicateWithSet('abcabc'));
+console.log(
+  "Using Set (Fastest and Clean) |",
+  RemoveDuplicateWithSet("abcabc"),
+);
 
 //// 2. Using a Set + for...of (More Control And Recommended For Interview)
-
 function RemoveDublicateWithSetAndForOf(str) {
-    let seen = new Set();
-    for (let i = 0; i < str.length; i++) {
-       if (! seen.has(str[i])) {
-          seen.add(str[i])
-       }
+  let seen = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (!seen.has(str[i])) {
+      seen.add(str[i]);
     }
-    return [...seen].join('')
+  }
+  return [...seen].join("");
 }
-console.log('RemoveDublicateWith-Set-And-ForOf |', RemoveDublicateWithSetAndForOf('abcabc'));
+console.log(
+  "RemoveDublicateWith-Set-And-ForOf |",
+  RemoveDublicateWithSetAndForOf("abcabc"),
+);
+
+//// 3. Using an Object as a Frequency Map
+function RemoveDublicateWithObj(str) {
+  let seen = {};
+  let result = "";
+  for (const char of str) {
+    if (!seen[char]) {
+      seen[char] = true;
+      result += char;
+    }
+  }
+  return result;
+}
+console.log("RemoveDublicateWith-Obj |", RemoveDublicateWithObj("abcabc"));
