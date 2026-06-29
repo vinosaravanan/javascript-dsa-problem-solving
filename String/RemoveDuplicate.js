ef912f911aff1653915a6494f2650d3892303c11
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-06-29 17:46:40 +0530

    RemoveDuplicate with set and For of

--- String/RemoveDuplicate.js
+++ String/RemoveDuplicate.js
@@ -11,3 +11,16 @@ function RemoveDuplicateWithSet(str) {
     return [...new Set(str)].join('')
 }
 console.log('Using Set (Fastest and Clean) |', RemoveDuplicateWithSet('abcabc'));
+
+//// 2. Using a Set + for...of (More Control And Recommended For Interview)
+
+function RemoveDublicateWithSetAndForOf(str) {
+    let seen = new Set();
+    for (let i = 0; i < str.length; i++) {
+       if (! seen.has(str[i])) {
+          seen.add(str[i])
+       }
+    }
+    return [...seen].join('')
+}
+console.log('RemoveDublicateWith-Set-And-ForOf |', RemoveDublicateWithSetAndForOf('abcabc'));
