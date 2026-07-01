853b437bbb5700f68582317be0cbdeadc35b9f58
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-01 09:08:41 +0530

    RemoveGivenCharaterWithSplit

--- String/RemoveGivenCharacterStr.js
+++ String/RemoveGivenCharacterStr.js
@@ -3,7 +3,6 @@
 //// input - "banana", "a";
 //// output - "bnn";
 
-
 // 1. Brute Force (Using Loop + +=)
 function RemoveGivenCharacterInStr(str, target) {
     let result = '';
@@ -16,5 +15,20 @@ function RemoveGivenCharacterInStr(str, target) {
 }
 console.log('emoveGivenCharacterInStr-Brute-force |', RemoveGivenCharacterInStr('banana', 'a'));
 
+//  2. Optimized Loop Using Array + join()  Recommended for Interviews
+function RemoveGivenCharacterwithArr(str, target) {
+   let result = [];
+   for (let i = 0; i < str.length; i++) {
+      if (str[i] !== target) {
+          result.push(str[i])
+      }
+   }
+   return result.join('')
+}
+console.log('RemoveGivenCharacterwith-Arr |', RemoveGivenCharacterwithArr("banana", 'a'));
 
-
+// 3. Using split() + join() (One-liner Approach)
+function RemoveGivenCharacterWithSplit(str, target) {
+   return str.split(target).join('')
+}
+console.log('RemoveGivenCharacterWith-Split |', RemoveGivenCharacterWithSplit('banana', 'a'));
