86c7d79a46c0a92713de715b7ed51ea7281ab71d
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-04 14:28:05 +0530

    RemoveDuplicatesin-place.js

--- String/RemoveDuplicatesin-place.js
+++ String/RemoveDuplicatesin-place.js
@@ -6,4 +6,25 @@
 //// Modify the array in-place (using a write pointer technique).
 //// Join the array back into a string at the end.
 
+/// with set mrthod
+function removeDuplicatesWithSet(str) {
+  const chars = str.split('');   
+  const seen = new Set();
+  let writeIndex = 0;          
 
+  for (let readIndex = 0; readIndex < chars.length; readIndex++) {
+    const char = chars[readIndex];
+    if (!seen.has(char)) {
+      seen.add(char);
+      chars[writeIndex] = char;  
+      writeIndex++;
+    }
+  }
+
+  chars.length = writeIndex;     
+  return chars.join('');
+}
+
+console.log('removeDuplicatesWith-Set-Method |',removeDuplicatesWithSet("aabbccddeeff")); // "abcdef"
+console.log('removeDuplicatesWith-Set-Method |',removeDuplicatesWithSet("programming"));  // "progamin"
+console.log('removeDuplicatesWith-Set-Method |', removeDuplicatesWithSet("mississippi"));  // "misp"
