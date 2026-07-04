1f6a5377561b0abc81d9e1640dca454340a8758b
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-04 14:23:27 +0530

    RemoveDuplicatesin-place.js

---
+++ String/RemoveDuplicatesin-place.js
@@ -0,0 +1,9 @@
+////        Remove Duplicates from a String (In-Place)
+
+//// Since true in-place string mutation isn't possible in JS, the standard approach is to:
+
+//// Convert the string into an array of characters (arrays are mutable).
+//// Modify the array in-place (using a write pointer technique).
+//// Join the array back into a string at the end.
+
+
