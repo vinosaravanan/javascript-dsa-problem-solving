a2874dcf45f0224290f089e15c2dfa3aa77da8da
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-23 23:53:02 +0530

    remove dublicate

---
+++ Array/RemoveDuplicatesInArr.js
@@ -0,0 +1,10 @@
+/////              How do remove duplicates from a given array?
+////  input - [1, 1, 2, 3, 2, 4, 5, 5]
+////  output - [1, 2, 3, 4, 5]
+
+function RemoveDublicateWithObj(arr){
+   let unique = []
+
+}
+console.log('RemoveDublicateWith-Loop-Obj',RemoveDublicateWithObj([1, 1, 2, 3, 2, 4, 5, 5]));
+
