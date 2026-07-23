8b7da3afe57d929cedaba24d176d3685db578f81
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-23 23:59:16 +0530

    remove dublicate

--- Array/RemoveDuplicatesInArr.js
+++ Array/RemoveDuplicatesInArr.js
@@ -2,6 +2,7 @@
 ////  input - [1, 1, 2, 3, 2, 4, 5, 5]
 ////  output - [1, 2, 3, 4, 5]
 
+/// Approche 1 pure loop with Obj
 function RemoveDublicateWithObj(arr) {
   let unique = [];
   let seen = {};
@@ -17,3 +18,11 @@ console.log(
   "RemoveDublicateWith-Loop-Obj",
   RemoveDublicateWithObj([1, 1, 2, 3, 2, 4, 5, 5]),
 );
+
+/// Aproche 2 Using Set method  Set is a collection of unique values.
+
+const setarr = [1, 1, 2, 3, 2, 4, 5, 5];
+
+const setRes = [...new Set(setarr)] // set method only take uniqie value 
+console.log('set method ', setRes);
+
