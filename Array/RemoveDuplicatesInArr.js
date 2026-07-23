9bdf40319202f24c03ccfe44846265127eb6c9fc
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-07-23 23:57:12 +0530

    remove dublicate

--- Array/RemoveDuplicatesInArr.js
+++ Array/RemoveDuplicatesInArr.js
@@ -2,9 +2,18 @@
 ////  input - [1, 1, 2, 3, 2, 4, 5, 5]
 ////  output - [1, 2, 3, 4, 5]
 
-function RemoveDublicateWithObj(arr){
-   let unique = []
-
+function RemoveDublicateWithObj(arr) {
+  let unique = [];
+  let seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!seen[arr[i]]) {
+      unique.push(arr[i]);
+      seen[arr[i]] = true;
+    }
+  }
+  return unique;
 }
-console.log('RemoveDublicateWith-Loop-Obj',RemoveDublicateWithObj([1, 1, 2, 3, 2, 4, 5, 5]));
-
+console.log(
+  "RemoveDublicateWith-Loop-Obj",
+  RemoveDublicateWithObj([1, 1, 2, 3, 2, 4, 5, 5]),
+);
