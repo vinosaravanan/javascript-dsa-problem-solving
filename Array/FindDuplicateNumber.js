6bc238869169dd32e32894a4bf357a7a7c79265d
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-02 13:18:50 +0530

    FindDuplicateNumber.js

--- Array/FindDuplicateNumber.js
+++ Array/FindDuplicateNumber.js
@@ -16,3 +16,34 @@ console.log(
   "FindDublicateWithNestedLoops |",
   FindDublicateWithNestedLoops([1, 3, 4, 2, 2]),
 );
+////// Approach 2 : Using Sorting
+function FindDublicateWithSorting(nums){
+   for (let i = 1; i < nums.length; i++) {
+       if (nums[i] === nums[i - 1]) {
+          return nums[i]
+       }
+   }
+}
+console.log(
+  "FindDublicateWithSorting |",
+ FindDublicateWithSorting([1, 3, 4, 2, 2]),
+);
+//////////// Approach 3 : Using  Using Set method
+function FindDublicateWithSetMethod(nums){
+    let seen = new Set();
+    for (const num of nums) {
+         if (seen.has(num)) {
+            return num
+         }
+         seen.add(num)
+    }
+}
+console.log(
+  "FindDublicateWithSetMethod |",
+ FindDublicateWithSetMethod([1, 3, 4, 2, 2]),
+);
+
+
+
+
+
