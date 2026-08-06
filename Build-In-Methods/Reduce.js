138a717e87f28fbd64b6659270552e9368940a09
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-06 22:52:07 +0530

    add problem

--- Build-In-Methods/Reduce.js
+++ Build-In-Methods/Reduce.js
@@ -24,4 +24,20 @@ function CountTotalQuantity(items){
 }
 console.log('CountTotalQuantity |', CountTotalQuantity([{ item: "Apple", qty: 3 },{ item: "Orange", qty: 4 },{ item: "Mango", qty: 2 }]));
 
+////   3. Find Most Expensive Product
+// const products = [
+//   { id: 1, name: "Mouse", price: 1500 },
+//   { id: 2, name: "Keyboard", price: 3500 },
+//   { id: 3, name: "Laptop", price: 50000 },
+//   { id: 4, name: "Monitor", price: 15000 },
+//   { id: 5, name: "Headphones", price: 2500 }
+// ];
+////// // Output {name: "Laptop", price: 50000}
 
+function MostExpensiveProduct(products){
+  let res = products.reduce((max, product) => {
+      return product.price > max.price ? product : max
+  }, products[0]);
+  return res
+}
+console.log('MostExpensiveProduct |', MostExpensiveProduct([{ id: 1, name: "Mouse", price: 1500 },  { id: 2, name: "Keyboard", price: 3500 },  { id: 3, name: "Laptop", price: 50000 },{ id: 4, name: "Monitor", price: 15000 },{ id: 5, name: "Headphones", price: 2500 } ]));
