15c40cccb5e04b742cfae578cdb1cf8b9eb1013b
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-06 16:51:19 +0530

    add problem

--- Build-In-Methods/Reduce.js
+++ Build-In-Methods/Reduce.js
@@ -11,3 +11,17 @@ function CalculateTotalPrice(cart){
   return res
 }
 console.log('CalculateTotalPrice |', CalculateTotalPrice([{ name: "Laptop", price: 50000, qty: 2 }, { name: "Mouse", price: 1000, qty: 3 }, { name: "Keyboard", price: 2500, qty: 1 }]));
+
+//////  2. Count Total Quantity
+///  [{ item: "Apple", qty: 3 },{ item: "Orange", qty: 4 },{ item: "Mango", qty: 2 }]
+///  Output 9
+
+function CountTotalQuantity(items){
+  let res = items.reduce((acc, item) => {
+     return acc + item.qty
+  }, 0)
+  return res
+}
+console.log('CountTotalQuantity |', CountTotalQuantity([{ item: "Apple", qty: 3 },{ item: "Orange", qty: 4 },{ item: "Mango", qty: 2 }]));
+
+
