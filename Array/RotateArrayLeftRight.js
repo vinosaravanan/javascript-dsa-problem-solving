93ae19d50c83ca1b62485f606d6a27e31861977c
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-09 22:32:06 +0530

    problem added

---
+++ Array/RotateArrayLeftRight.js
@@ -0,0 +1,44 @@
+//////        How to rotate an array left and right by a given number K?
+
+// K times rotation means rotating an array exactly K times 
+// The number K K determines how many steps (or positions) the elements in the array are shifted.
+
+// Example: Rotate an Array Left by K=2 K = 2 K=2
+// If the array is:
+
+// [1,2,3,4,5][
+// After 1 left rotation:
+// [2,3,4,5,1]
+// After 2 left rotations:
+// [3,4,5,1,2]
+
+// Example: Rotate an Array Right by K=2K = 2K=2
+// If the array is:
+// [1,2,3,4,5]
+// After 1 right rotation:
+// [5,1,2,3,4]
+// After 2 right rotations:
+// [4,5,1,2,3]
+
+//    input - const arr = [1, 2, 3, 4, 5, 6, 7];
+//               const K = 3;
+//         Output:
+//          Left Rotation by K   
+//          K: [4, 5, 6, 7, 1, 2, 3] 
+//           Right Rotation by K
+//          K: [5, 6, 7, 1, 2, 3, 4]
+
+
+
+/// Approche 1 : Using Slice method 
+function RotateArrLeftAndRight(arr, k, direaction){
+   let n = arr.length;
+   if (direaction === 'Left') {
+    return [...arr.slice(k), ...arr.slice(0, k)]
+   } else if(direaction === "Right"){
+      return [...arr.slice(n-3), ...arr.slice(0, n-3)]
+   }
+     
+}
+console.log('RotateArrLeftAndRight-Left |', RotateArrLeftAndRight([1, 2, 3, 4, 5, 6, 7], 3, 'Left'));
+console.log('RotateArrLeftAndRight-Right |', RotateArrLeftAndRight([1, 2, 3, 4, 5, 6, 7], 3, 'Right'));
