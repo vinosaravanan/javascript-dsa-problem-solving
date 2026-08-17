819481cc5ed2ed8881ccba4cd1502e9f305d4c18
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-17 22:34:03 +0530

    code added

---
+++ oops/projects-In-oops/bank-account-oop/Transaction.js
@@ -0,0 +1,11 @@
+class Transaction {
+    constructor(type, amount, balanceAfter) {
+        this.id = Math.floor(Math.random() * 100000);
+        this.type = type;
+        this.data = new Date();
+        this.amount = amount;
+        this.balanceAfter = balanceAfter;
+    }
+
+    
+}
