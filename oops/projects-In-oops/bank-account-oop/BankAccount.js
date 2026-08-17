80deb6e4f3a103f2dcb03067986b7f12d7a6b5d4
vinosaravanan <152616485+vinosaravanan@users.noreply.github.com>
2026-08-17 22:45:07 +0530

    code added

--- oops/projects-In-oops/bank-account-oop/BankAccount.js
+++ oops/projects-In-oops/bank-account-oop/BankAccount.js
@@ -1,8 +1,34 @@
+const Transaction = require('./Transaction');
 class BankAccount {
     #Balance;
-    #Transation = []
+    #Transation;
     constructor(owner, initialBalance = 0) {
+         this.accountNumber = Math.floor(Math.random() * 100000)
          this.owner = owner;
-         this.#Balance = initialBalance
+         this.#Balance = initialBalance;
+         this.#Transation = []
     }
+
+    Deposit(amount){
+      if (amount <= 0) {
+        throw new Error("deposit must be positive");
+      }
+      this.#Balance += amount;
+      this.Transaction.push(new Transaction('deposit', amount, this.#Balance))
+      return this.#Balance
+    }
+
+    withdraw(amount){
+        if (amount <= 0) {
+            throw new Error("withdraw must be positive");
+        }
+        if (amount > this.#Balance) {
+            throw new Error('inEfficient fund')
+        }
+
+        this.#Balance -= amount;
+        this.#Transation.push(new Transaction('Withdraw', amount, this.#Balance));
+        return this.#Balance
+    }
+
 }
